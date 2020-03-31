97ed68cdd640b6610e4515eb34dbe0e749d6b81f
Ricardo Corrie <rcorrie90@gmail.com>
2020-03-30 22:01:39 -0400

    fixes issue with index.ios

--- index.ios.js
+++ index.ios.js
@@ -5,7 +5,7 @@
 import { NativeModules, requireNativeComponent } from 'react-native';
 
 export const JitsiMeetView = requireNativeComponent('RNJitsiMeetView');
-export const JitsiMeetModule = NativeModules.RNJitsiMeetModule
+export const JitsiMeetModule = NativeModules.RNJitsiMeetView;
 const call = JitsiMeetModule.call;
 const audioCall = JitsiMeetModule.audioCall;
 JitsiMeetModule.call = (url, userInfo) => {
